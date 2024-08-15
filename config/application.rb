5fcf1bc679785fec82919d034b4ed332c12b9a4e
pham.tuan.anhc <tuananhchums23@gmail.com>
2024-08-15 10:31:07 +0700

    Base_layout_I18n_Title

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,8 @@ Bundler.require(*Rails.groups)
 module RailsTutorial
   class Application < Rails::Application
     config.load_defaults 7.0
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
   end
 end
