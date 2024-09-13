638030d7267c7b8057ee57479b4cee7cbf7f7198
pham.tuan.anhc <tuananhchums23@gmail.com>
2024-09-13 13:26:31 +0700

    rspec test room controller crud

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,6 +3,8 @@ class Room < ApplicationRecord
   enum status: {active: 0, inactive: 1}
   belongs_to :room_type
   has_many :requests, dependent: :destroy
+  validates :room_number, presence: true
+  validates :status, presence: true
 
   scope :excluding_ids, ->(ids){where.not(id: ids)}
   scope :by_ids, ->(ids){where(id: ids)}
--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :room do
     room_number { 101 }
     status { :active }
+    description { "A standard room." }
     association :room_type
   end
 end
